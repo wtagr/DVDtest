7f6db320729cc56593e45955cdd52095059f957c
meng <xumeng818@gmail.com>
2019-03-24 22:51:24 +0200

    0.1

--- R/DVDtest-package.R
+++ R/DVDtest-package.R
@@ -2,26 +2,14 @@
 
 #' Difference between Varying Distributions Test (DVDtest)
 #' 
-#' c("\\Sexpr[results=rd,stage=build]{tools:::Rd_package_description(\"#1\")}",
-#' "DVDtest")\Sexpr{tools:::Rd_package_description("DVDtest")}
+#' This package contains functions of a test on the difference between varying distributions. 
 #' 
-#' 
-#' The DESCRIPTION file:
-#' c("\\Sexpr[results=rd,stage=build]{tools:::Rd_package_DESCRIPTION(\"#1\")}",
-#' "DVDtest")\Sexpr{tools:::Rd_package_DESCRIPTION("DVDtest")}
-#' c("\\Sexpr[results=rd,stage=build]{tools:::Rd_package_indices(\"#1\")}",
-#' "DVDtest")\Sexpr{tools:::Rd_package_indices("DVDtest")} ~~ An overview of
-#' how to use the package, including the most important functions ~~
-#' 
-#' @name Package
-#' @aliases DVDtest-package
+#' @name DVDtest-package
 #' @docType package
 #' @author Meng Xu, Philip Reiss
 #' 
-#' Maintainer: Meng Xu
 #' @seealso \code{\link{DVDtest}}
 #' @references reiss-EMR18.pdf
-#' @keywords package
 #' 
 #' 
 #' 
