9806c86dd6d84c9475a434fda39fd4c2ca7bc94d
meng <xumeng818@gmail.com>
2019-03-24 22:43:45 +0200

    0.1

--- R/DVDtest-package.R
+++ R/DVDtest-package.R
@@ -1,7 +1,6 @@
 
 
-#' c("\\Sexpr[results=rd,stage=build]{tools:::Rd_package_title(\"#1\")}",
-#' "DVDtest")\Sexpr{tools:::Rd_package_title("DVDtest")}
+#' Difference between Varying Distributions Test (DVDtest)
 #' 
 #' c("\\Sexpr[results=rd,stage=build]{tools:::Rd_package_description(\"#1\")}",
 #' "DVDtest")\Sexpr{tools:::Rd_package_description("DVDtest")}
@@ -14,17 +13,12 @@
 #' "DVDtest")\Sexpr{tools:::Rd_package_indices("DVDtest")} ~~ An overview of
 #' how to use the package, including the most important functions ~~
 #' 
-#' @name DVDtest-package
+#' @name Package
 #' @aliases DVDtest-package
-#' @aliases DVDtest
 #' @docType package
-#' @author
-#' c("\\Sexpr[results=rd,stage=build]{tools:::Rd_package_author(\"#1\")}",
-#' "DVDtest")\Sexpr{tools:::Rd_package_author("DVDtest")}
+#' @author Meng Xu, Philip Reiss
 #' 
-#' Maintainer:
-#' c("\\Sexpr[results=rd,stage=build]{tools:::Rd_package_maintainer(\"#1\")}",
-#' "DVDtest")\Sexpr{tools:::Rd_package_maintainer("DVDtest")}
+#' Maintainer: Meng Xu
 #' @seealso \code{\link{DVDtest}}
 #' @references reiss-EMR18.pdf
 #' @keywords package
