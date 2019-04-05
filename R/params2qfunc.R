0c05e2feba7047ab21ad981f63220ba339b84b0e
meng <xumeng818@gmail.com>
2019-04-05 23:26:02 +0300

    0.1

--- R/params2qfunc.R
+++ R/params2qfunc.R
@@ -16,15 +16,20 @@ function(params, family, dist.method) {
   if (dist.method == 'wass'){
     if ("gaulss" %in% family) return(function(p) qnorm(p, mean=mu, sd=sigma))
     else if ("NO" %in% family) return(function(p) qnorm(p, mean=mu, sd=sigma))
-    else if ("BCCG" %in% family) return(function(p) qBCCG(p, mu=mu, sigma=sigma, nu=nu))
-    else if ("BCT" %in% family) return(function(p) qBCT(p, mu=mu, sigma=sigma, nu=nu, tau=tau))
-    else if ("BCPE" %in% family) return(function(p) qBCPE(p, mu=mu, sigma=sigma, nu=nu, tau=tau))
+    else {
+      qfun. <- match.fun(family[1])
+      qfun <- paste0("q",qfun.)
+      return(function(p) qfun(p, mu=mu, sigma=sigma, nu=nu, tau=tau))
+    }
   }
+
   if (any(dist.method == c('L2','L1','Hellinger'))) {
     if ("gaulss" %in% family) return(function(x) dnorm(x, mean=mu, sd=sigma))
     else if ("NO" %in% family) return(function(x) dnorm(x, mean=mu, sd=sigma))
-    else if ("BCCG" %in% family) return(function(x) dBCCG(x, mu=mu, sigma=sigma, nu=nu))
-    else if ("BCT" %in% family) return(function(x) dBCT(x, mu=mu, sigma=sigma, nu=nu, tau=tau))
-    else if ("BCPE" %in% family) return(function(x) dBCPE(x, mu=mu, sigma=sigma, nu=nu, tau=tau))
+    else {
+      pfun. <- match.fun(family[1])
+      pfun <- paste0("d",pfun.)
+      return(function(x) pfun(x, mu=mu, sigma=sigma, nu=nu, tau=tau))
+    }
   }
-}
+  }
